ddb64989691135e082ce3261d28506258e56097c
MagedTT <maged922001@gmail.com>
2025-10-31 19:35:34 +0200

    Add Validation Logic to The Forms

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,11 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from "@angular/router";
+import ValidateForm from '../../helpers/validatorForm';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  isPassword: boolean = true;
+  eyeIcon: string = 'fa fa-eye-slash';
+  passwordInputType: string = 'password';
+  loginForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required, Validators.minLength(8)]]
+    });
+  }
+
+  togglePassword() {
+    if (this.isPassword) {
+      this.eyeIcon = 'fa fa-eye-slash';
+      this.passwordInputType = 'password';
+    } else {
+      this.eyeIcon = 'fa fa-eye';
+      this.passwordInputType = 'text';
+    }
+    this.isPassword = !this.isPassword;
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log(this.loginForm.value);
+    } else {
+      ValidateForm.validateAllFormFields(this.loginForm);
+    }
+  }
 }
--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,11 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import validateForm from '../../helpers/validatorForm';
+
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    RouterLink,
+    ReactiveFormsModule
+  ],
   templateUrl: './signup.component.html',
   styleUrl: './signup.component.css'
 })
-export class SignupComponent {
+export class SignupComponent implements OnInit {
+  isPassword: boolean = true;
+  eyeIcon: string = 'fa fa-eye-slash';
+  passwordInputType: string = 'password';
 
+  signUpForm!: FormGroup;
+
+  constructor(private fb: FormBuilder) { }
+  
+  ngOnInit(): void {
+    this.signUpForm = this.fb.group({
+      firstname: ['', [Validators.required, Validators.minLength(3)]],
+      lastname: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]],
+      username: ['', Validators.required],
+      password: ['', Validators.required, Validators.minLength(8)],
+    });
+  }
+
+  togglePassword() {
+    if (this.isPassword) {
+      this.eyeIcon = 'fa fa-eye-slash';
+      this.passwordInputType = 'password';
+    } else {
+      this.eyeIcon = 'fa fa-eye';
+      this.passwordInputType = 'text';
+    }
+    this.isPassword = !this.isPassword;
+  }
+
+  onSubmit() {
+    if (this.signUpForm.valid) {
+      console.log(this.signUpForm.value);
+    } else {
+      validateForm.validateAllFormFields(this.signUpForm);
+    }
+  }
 }
